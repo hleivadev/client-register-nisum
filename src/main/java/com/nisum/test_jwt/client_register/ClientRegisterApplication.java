90a4bef1b3ef7a25233e196b6e34d29cae6b965b
Hector Leiva <hleivadev@gmail.com>
2025-04-28 12:09:49 -0400

    commit inicial

---
+++ src/main/java/com/nisum/test_jwt/client_register/ClientRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.test_jwt.client_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisum/test_jwt/client_register/ClientRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.test_jwt.client_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
